3eeb0b610283f90e2abe59039cbdd0fdfa83439c
Amine <amine@pixalione.com>
2018-09-24 12:56:36 +0200

    Initial commit

---
+++ nodeJsApp/index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var os = require('os');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`<h1>I'm ${os.hostname()}</h1>`);
+}).listen(8080);
